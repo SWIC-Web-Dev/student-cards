0cb26844a244f0cf1251aa6735f0fd1326e90925
Manav Misra <manavm@visionwebsoft.com>
2024-09-30 20:24:46 -0500

    Add code from class

--- src/components/card.js
+++ src/components/card.js
@@ -1,21 +1,9 @@
-export default function Card() {
-  return `<section>
-      <div
-        class="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
-      >
-        <div class="rounded bg-white p-4 shadow">
-          <h2 class="text-xl font-semibold">Amanda Ellis</h2>
-          <ul class="ml-4 list-disc space-y-1">
-            <li>This is the third college I've attended</li>
-            <li>I am a wife and mom</li>
-            <li>I have been a dental assistant for 16 years</li>
-            <li>I love dogs, books, and hiking</li>
-            <li>
-              I not so secretly love late 90s and early 2000s pop-punk and emo
-              music
-            </li>
-          </ul>
-        </div>
-      </div>
+export default function Card(person) {
+  return `<section class="flex flex-col gap-4 rounded p-4 shadow text-slate-900 bg-slate-200">
+      <h2 class="text-xl font-semibold">${person.name}</h2>
+      
+      <ul class="ml-4 list-disc space-y-1">
+        ${person.facts.map((fact) => `<li>${fact}</li>`).join("\n")}
+      </ul>
     </section>`;
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import data from "../db/db.json";
+import Card from "./components/card";
 
-document.getElementById("app").innerHTML =
-  `<p class="text-red-500 text-3xl">Hello from JavaScript!</p>`;
+document.getElementById("app").innerHTML = `
+  <main class="flex flex-col items-center gap-y-8">
+    <button type="button" class="bg-orange-500 px-16 py-8 text-3xl rounded-xl hover:bg-orange-700">Select Student</button>
+    
+    <section class="grid grid-cols-1 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4">${data.map((d) => Card(d)).join("\n")}</section>
+  </main>
+`;
+
+const studentSections = document.querySelectorAll("section > section");
+
+document.querySelector("button").addEventListener("click", () => {
+  const currentChosen = document.querySelector(".chosen");
+  if (currentChosen) currentChosen.classList.remove("chosen");
+
+  const randomIndex = Math.floor(Math.random() * data.length);
+  const randomStudentSection = studentSections[randomIndex];
+
+  randomStudentSection.classList.add("chosen");
+});
